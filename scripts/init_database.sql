8443d0c49f050d1e2bc5ea0ecede6d1513ffffd4
Akshat29-shah <shah.akshathi@northeastern.edu>
2026-04-07 12:51:42 -0400

    Fix comments and database name typos in SQL script

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -17,7 +17,7 @@ and ensure you have proper backups before running this script.
 USE master;
 GO
 
-.. Drop and recreate the 'DataWarehouse' database
+-- Drop and recreate the 'DataWarehouse' database
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Datalarehouse')
 BEGIN
 ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
@@ -25,7 +25,7 @@ DROP DATABASE DataWarchouse;
 END
 GO
 
--. Create the 'Datalarehouse' database
+-- Create the 'Datalarehouse' database
 CREATE DATABASE DataWarehouse;
 GO
 
